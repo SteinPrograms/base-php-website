775480217d362dcf3a94dd6649d3e3b723424457
Hugo Demenez <hdemenez@hotmail.fr>
2021-12-14 14:35:59 +0100

    commit

--- backend/login_verification.php
+++ backend/login_verification.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 'on');
 if(isset($_POST['username']) && isset($_POST['password'])){
    $username = $_POST['username'];
    $password = $_POST['password'];
-   require __DIR__ . '/database.php';
+   require_once('database.php');
    $conn = cnxDB();
    $results = $conn->query("SELECT * FROM user WHERE username = '$username'");
    $row = $results->fetch_assoc();
