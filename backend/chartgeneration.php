aff22d382492ab73e7b85533487d54a3945f9aff
Hugo Demenez <hdemenez@hotmail.fr>
2021-12-14 15:27:48 +0100

    commit

--- backend/chartgeneration.php
+++ backend/chartgeneration.php
@@ -7,7 +7,14 @@ function chart($name,$dates,$values){
     echo('<center><div style="width:75%;height:50%">
     <canvas id="'.$name.'"></canvas>
     <script>
-    const '.$name.' = document.getElementById("'.$name.'").getContext("2d");
+    
+    var '.$name.' = document.getElementById("'.$name.'").getContext("2d");
+    var gradient = chart.createLinearGradient(0, 0, 0, 450);
+    gradient.addColorStop(0, "rgba(255, 0,0, 0.5)");
+    gradient.addColorStop(0.5, "rgba(255, 0, 0, 0.25)");
+    gradient.addColorStop(1, "rgba(255, 0, 0, 0)");
+
+
     new Chart('.$name.', {
         type: "line",
         data: {
@@ -15,7 +22,7 @@ function chart($name,$dates,$values){
             datasets: [{
                 label: "Evolution du portfolio : '.$name.'",
                 data: '.$values.',
-                backgroundColor: "rgba('.$random_color1.', '.$random_color2.', '.$random_color3.', 1)",
+                backgroundColor: gradient,
                 borderColor: "rgba('.$random_color1.', '.$random_color2.', '.$random_color3.', 1)",
                 borderWidth: 3
             }]
