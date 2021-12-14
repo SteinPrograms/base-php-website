8ad6daf3ebce731ab0fa7f373117b77ab1a1415e
Hugo Demenez <hdemenez@hotmail.fr>
2021-12-14 13:18:42 +0100

    commit

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
-<?php
-
-echo "hello world";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TradingWebsiteTracker</title>
+</head>
+<body>
+    <h1>Welcome</h1>
+    <? 
+    $username = $this->session->userdata('username');
+    echo $username;
+    ?>
+</body>
+</html>
