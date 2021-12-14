7a2a498db30337fbb8315bb15321a1f4aa467f42
Hugo Demenez <hdemenez@hotmail.fr>
2021-12-14 14:43:40 +0100

    commmit

--- backend/login_verification.php
+++ backend/login_verification.php
@@ -4,7 +4,18 @@ require_once('database.php');
 if(isset($_POST['username']) && isset($_POST['password'])){
    $username = $_POST['username'];
    $password = $_POST['password'];
-   
+   function cnxDB(){
+      $connexion = mysqli_connect("localhost", "hugodemenez", "password" , "database") ;
+
+      // si jamais la connexion n'était obtenue
+      if ( ! $connexion )  
+      {
+          die("Connexion impossible : " . mysqli_connect_error());  // fin du programme en affichant un message d'erreur
+      } 
+      else{
+          return $connexion;
+      }
+  }
    $conn = cnxDB();
    $results = $conn->query("SELECT * FROM user WHERE username = '$username'");
    $row = $results->fetch_assoc();
