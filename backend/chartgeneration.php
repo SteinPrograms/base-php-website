0fd14c9cfe4b0e34d473736880c69fb0a631d059
Hugo Demenez <hdemenez@hotmail.fr>
2021-12-14 15:51:44 +0100

    commit

--- backend/chartgeneration.php
+++ backend/chartgeneration.php
@@ -10,7 +10,7 @@ function chart($name,$dates,$values){
     var '.$name.' = document.getElementById("'.$name.'").getContext("2d"),
         gradient = '.$name.'.createLinearGradient(0, 0, 0, 450);
 
-    gradient.addColorStop(0, "#0E4623");
+    gradient.addColorStop(0, "#B4E845");
     gradient.addColorStop(1, "rgba(0, 0, 0, 0)");
 
 
@@ -22,7 +22,7 @@ function chart($name,$dates,$values){
                 label: "Evolution du portfolio : '.$name.'",
                 data: '.$values.',
                 backgroundColor: gradient,
-                borderColor: "#0E4623",
+                borderColor: "#B4E845",
                 borderWidth: 3,
                 fill: true
             }]
