b359f8be5110a30f7cf2d78b41230d75e147984a
Hugo Demenez <hdemenez@hotmail.fr>
2021-12-21 18:13:05 +0100

    commit

--- frontend/home.php
+++ frontend/home.php
@@ -39,8 +39,19 @@
     </div>
 
 <div class="task-bar">
-    <a href="./home.php"><i class="fas fa-home"></i></a>
-    <a href="./settings.php"><i class="fas fa-cogs"></i></a>
+    <a href="./home.php">
+        <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none">
+            <path fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 19v-8.5a1 1 0 0 0-.4-.8l-7-5.25a1 1 0 0 0-1.2 0l-7 5.25a1 1 0 0 0-.4.8V19a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1z"/>
+        </svg>
+    </a>
+    <a href="./graphs.php">
+        <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none">
+            <path fill="currentColor" fill-rule="evenodd" d="M3 6a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6zm10 2a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0V8zm-4 3a1 1 0 1 0-2 0v5a1 1 0 1 0 2 0v-5zm8 3a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0v-2z" clip-rule="evenodd"/>
+        </svg>
+    </a>
+    <a href="./chat.php"></a>
+    <a href="./funds.php"></a>
+
 </div>
 
 </body>
