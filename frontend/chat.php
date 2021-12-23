47012670bae71e90b25fa263fc090f373d11a79c
Hugo Demenez <hdemenez@hotmail.fr>
2021-12-23 17:08:15 +0100

    commit

--- frontend/chat.php
+++ frontend/chat.php
@@ -13,7 +13,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1">
     <title>PortfolioTracker  | Chat</title>
     <link rel="stylesheet" href="../styles/chat.css?v=1.1">
     <link rel="shortcut icon" type="image/x-icon" href="../assets/Icon.svg"/>
--- frontend/home.php
+++ frontend/home.php
@@ -13,7 +13,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1">
     <title>PortfolioTracker  | Home</title>
     <link rel="stylesheet" href="../styles/home.css?v=1.7">
     <link rel="shortcut icon" type="image/x-icon" href="../assets/Icon.svg"/>
