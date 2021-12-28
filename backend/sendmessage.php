0ca77d4669cf24a7ac21ea62079b2817ebdc7da8
Hugo Demenez <hdemenez@hotmail.fr>
2021-12-28 15:56:44 +0100

    commit

---
+++ backend/sendmessage.php
@@ -0,0 +1,10 @@
+<?php 
+    $message = $_GET['message'];
+    $token = "1813447727:AAHDPI54DetjXDDNFCMqtN-7phGvwNy9rqY";
+    $chat_id = "-431364858";
+    $url = "https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&text=$message";
+
+    $result = file_get_contents($url);
+
+    header('Location: ../frontend/chat.php');
+?>
