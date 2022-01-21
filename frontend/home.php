ba7d1c390d67852c785f68b3c7e6cf676b8ade0e
Hugo Demenez <hdemenez@hotmail.fr>
2022-01-21 18:28:32 +0100

    is it working ?

--- frontend/home.php
+++ frontend/home.php
@@ -105,6 +105,25 @@
 </footer>
 
 <script>
+    function dateDiff(date1, date2){
+        var diff = {}                           // Initialisation du retour
+        var tmp = date2 - date1;
+    
+        tmp = Math.floor(tmp/1000);             // Nombre de secondes entre les 2 dates
+        diff.sec = tmp % 60;                    // Extraction du nombre de secondes
+    
+        tmp = Math.floor((tmp-diff.sec)/60);    // Nombre de minutes (partie entière)
+        diff.min = tmp % 60;                    // Extraction du nombre de minutes
+    
+        tmp = Math.floor((tmp-diff.min)/60);    // Nombre d'heures (entières)
+        diff.hour = tmp % 24;                   // Extraction du nombre d'heures
+        
+        tmp = Math.floor((tmp-diff.hour)/24);   // Nombre de jours restants
+        diff.day = tmp;
+        
+        return diff;
+    }
+
     function currentTime() {
         <?php 
             $requete = "select * from program";
@@ -128,7 +147,9 @@
         var hour = date.getHours();
         var min = date.getMinutes();
         var sec = date.getSeconds();
-        document.getElementById("runtime").innerText = date-start_date;
+
+        var diff = dateDiff(start_date,date);
+        document.getElementById("runtime").innerText = " il y a "+diff.day+" jours, "+diff.hour+" heures, "+diff.min+" minutes et "+diff.sec+" secondes";
         var t = setTimeout(function(){ currentTime() }, 1000);
   }
   
