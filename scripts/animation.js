624f598944cf1b603971dc5cb248b6b248ef3cf6
Hugo Demenez <hdemenez@hotmail.fr>
2022-01-21 16:10:49 +0100

    animated

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,10 +1,14 @@
 var tl = gsap.timeline({duration:1});
 
-tl.from('.title',{opacity:0,y:50});
-tl.from('.subtitle',{opacity:0,y:50});
-tl.from(['input','#password-reveal'],{opacity:0,y:50});
+tl.fromTo('.title',{y:250},{y:0,delay:2},0);
+tl.from('.title',{scale:2,duration:2,ease:"bounce.out"},0);
 
 
-tl.from('.submit-btn',{opacity:0,y:50});
+
+tl.from('.subtitle',{opacity:0});
+tl.from(['input','#password-reveal'],{opacity:0});
+
+
+tl.from('.submit-btn',{opacity:0,x:50,ease:"expo"});
 
 
