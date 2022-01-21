4d899c5b3a51fffa763535bb38205dd0846b8029
Hugo Demenez <hdemenez@hotmail.fr>
2022-01-21 15:14:44 +0100

    commit

--- index.php
+++ index.php
@@ -4,60 +4,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name = "viewport" content = "width=device-width, minimum-scale=1.0, maximum-scale = 1.0, user-scalable = no">
-    <link rel="stylesheet" href="./styles/style.css?v=2.1" media="screen" type="text/css"/>
+    <link rel="stylesheet" href="./styles/style.css?v=2.251" media="screen" type="text/css"/>
     <script src="https://kit.fontawesome.com/ed342dc3ca.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"></script>
-    
+    <script src="/scripts/index.js"></script>
     <link rel="shortcut icon" type="image/x-icon" href="/assets/Icon.svg"/>
-    <link rel="stylesheet" href="../styles/background.css?v=23.">
     <title>PortfolioTracker</title>
 </head>
-<body>
+<body onload="change_mode()">
+    <!-- Dark Mode / Light Mode switch -->
+    <div class="mode_switch">
+    <label class="switch">
+        <input id="mode" type="checkbox" onClick="change_mode()">
+        <span class="slider round"></span>
+    </label>
+    </div>
     <!-- Affichage du formulaire de connexion et d'inscription -->
-        <form id="connexion" class="input-group" action="/backend/login_verification.php" method="POST">      
-        <h1>Bonjour!</h1>
-        <p>Bienvenue sur le site<br>de gestion du programme</p>
-        <?php
-            if(isset($_GET['erreur'])){
-                $err = $_GET['erreur'];
-                if($err==1 || $err==2)
-                    echo "<p style='color:red;'>⚠️Utilisateur ou mot de passe incorrect</p>";
-            }
-        ?>  
-            <input type="text" class="input-field" placeholder="Identifiant" name="username" required>
-
-            <input id="password" type="password" class="input-field" placeholder="Mot de passe" name="password">
-            <div id="password-reveal" onclick="reveal_password()">
-                <i id="eye" class="fas fa-eye"></i>
-                <p id="password-reveal-text">Afficher le mot de passe</p>
-            </div>
-            <button type="submit" class="submit-btn">Connexion</button>
-            
-        </form>
-        <script>
-            const tl = gsap.timeline({duration:3});
-            tl.fromTo('.input-field', {scale:2});
-        </script>
-        <script>
-
-
-        function reveal_password() {
-            var text = document.getElementById('password-reveal-text')
-            
-            var x = document.getElementById("password");
-            var y = document.getElementById("eye");
-            if (x.type === "password") {
-                x.type = "text";
-                y.className = "fas fa-eye-slash";
-                text.innerHTML = "Masquer le mot de passe"
-            } else {
-                x.type = "password";
-                y.className = "fas fa-eye";
-                text.innerHTML = "Afficher le mot de passe"
-            }
+    <form id="connexion" class="input-group" action="/backend/login_verification.php" method="POST">      
+    <h1>Bonjour!</h1>
+    <p>Bienvenue sur le site<br>de gestion du programme</p>
+    <?php
+        if(isset($_GET['erreur'])){
+            $err = $_GET['erreur'];
+            if($err==1 || $err==2)
+                echo "<p style='color:red;'>⚠️Utilisateur ou mot de passe incorrect</p>";
         }
-    </script>
+    ?>  
+        <input type="text" class="input-field" placeholder="Identifiant" name="username" required>
 
+        <input id="password" type="password" class="input-field" placeholder="Mot de passe" name="password">
+        <div id="password-reveal" onclick="reveal_password()">
+            <i id="eye" class="fas fa-eye"></i>
+            <p id="password-reveal-text">Afficher le mot de passe</p>
+        </div>
+        <button type="submit" class="submit-btn">Connexion</button>
+    </form>
 </body>
 </html>
 
---
+++ scripts/index.js
@@ -0,0 +1,63 @@
+
+
+function change_mode(){
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        var newColorScheme = "dark";
+    }
+
+    else{
+        var newColorScheme = "light";
+    }
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+        var newColorScheme = event.matches ? "dark" : "light";
+    });
+    
+
+
+    var body = document.querySelector('body');
+    var h1 = document.querySelector('h1');
+    var password_text = document.querySelector('#password-reveal-text');
+    var input_fields = document.querySelectorAll('.input-field');
+    
+    var eye = document.querySelector('#eye');
+    if (newColorScheme == "light" ){
+        body.style.background = '#fff';
+        h1.style.color = '#000';
+        password_text.style.color = '#000';
+        eye.style.color = '#000';
+        input_fields.forEach(function colorchange(input_field){
+            input_field.style.background = '#000';
+            input_field.style.color = '#fff';
+        });
+        
+    }
+    else{
+        input_fields.forEach(function colorchange(input_field){
+            input_field.style.background = '#fff';
+            input_field.style.color = '#000';
+        });
+        body.style.background = '#000';
+        h1.style.color = '#fff';
+        password_text.style.color = '#fff';
+        eye.style.color = '#fff';
+    }
+    
+}
+
+function reveal_password() {
+    var text = document.getElementById('password-reveal-text')
+    
+    var x = document.getElementById("password");
+    var y = document.getElementById("eye");
+    if (x.type === "password") {
+        x.type = "text";
+        y.className = "fas fa-eye-slash";
+        text.innerHTML = "Masquer le mot de passe"
+    } else {
+        x.type = "password";
+        y.className = "fas fa-eye";
+        text.innerHTML = "Afficher le mot de passe"
+    }
+}
+
